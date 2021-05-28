45674ae414dc2e9bdb7c0532a8efba781a87cda0
Lucas <lilujiajiajia@163.com>
2021-05-28 17:23:47 +0800

    backup

--- examples/dqn_cartpole.py
+++ examples/dqn_cartpole.py
@@ -17,7 +17,7 @@ ENV_NAME = 'CartPole-v0'
 env = gym.make(ENV_NAME)
 np.random.seed(123)
 env.seed(123)
-nb_actions = env.action_space.n
+nb_actions = env.action_space.n     # nb_actions = 2
 
 # Next, we build a very simple model.
 model = Sequential()
@@ -43,7 +43,7 @@ dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-dqn.fit(env, nb_steps=50000, visualize=True, verbose=2)
+dqn.fit(env, nb_steps=50000, visualize=False, verbose=2)
 
 # After training is done, we save the final weights.
 dqn.save_weights('dqn_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
